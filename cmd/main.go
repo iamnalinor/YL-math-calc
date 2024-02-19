2a4e9fdd8abce53c327f7e6ea53d6cd7cfe79992
Albert <63642425+iamnalinor@users.noreply.github.com>
2024-02-19 12:40:33 +0400

    Simplify main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"context"
 	"math-calc/internal/application"
 	"os"
 )
 
 func main() {
-	ctx := context.Background()
-	os.Exit(mainWithExitCode(ctx))
-}
-
-func mainWithExitCode(ctx context.Context) int {
 	app := application.NewApplication()
-	return app.Run(ctx)
+	os.Exit(app.Run())
 }
