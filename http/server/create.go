5137ca9249b84b1a58b01a05e04cda8cc98b6798
Albert <63642425+iamnalinor@users.noreply.github.com>
2024-02-21 14:43:31 +0400

    Implement expression parsing

--- http/server/create.go
+++ http/server/create.go
@@ -10,6 +10,7 @@ import (
 	"math-calc/internal/application"
 	"math-calc/internal/operation"
 	"net/http"
+	"strconv"
 )
 
 var usedIdempotentTokens = make(map[string]bool)
@@ -55,39 +56,114 @@ func createExpression(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	parseExpression(input.Expression)
-
 	app := r.Context().Value("app").(*application.Application)
 
 	app.Database.UpdatingMutex.Lock()
-	opMul, err := app.Database.Create(operation.Operation{
-		Op:    operation.Multiply,
-		Left:  2,
-		Right: 2,
-	})
 
-	opAdd, err := app.Database.Create(operation.Operation{
-		Op:               operation.Addition,
-		Left:             2,
-		RightOperationID: opMul,
-		Expression:       "2+2*2",
-	})
+	opId, err := parseExpression(input.Expression, app)
+	op, _ := app.Database.Get(opId)
+	op.Expression = input.Expression
+	app.Database.Update(op)
+
 	app.Database.UpdatingMutex.Unlock()
 
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "failed to parse expression: %s", err)
+		return
+
+	}
+
 	w.WriteHeader(http.StatusCreated)
-	data, err := json.Marshal(createOutput{Id: opAdd})
+	data, err := json.Marshal(createOutput{Id: opId})
 	if err != nil {
 		panic(err)
 	}
 	w.Write(data)
 }
 
-func parseExpression(expression string) (string, error) {
+func parseExpression(expression string, app *application.Application) (operation.ID, error) {
 	expr, err := parser.ParseExpr(expression)
 	if err != nil {
-		return "", fmt.Errorf("failed to parse expression: %w", err)
+		return 0, fmt.Errorf("failed to parse expression: %w", err)
 	}
 
-	ast.Print(token.NewFileSet(), expr)
-	return "", nil
+	result, err := unparseTree(expr, app)
+	if err != nil {
+		return 0, fmt.Errorf("failed to unparse expression: %w", err)
+	}
+
+	if result.OperationID == 0 {
+		return 0, fmt.Errorf("the expression does not contain any operations")
+	}
+
+	return result.OperationID, nil
+}
+
+type unparseResult struct {
+	OperationID operation.ID
+	Value       float64
+}
+
+var unparseResultEmpty = unparseResult{}
+
+func unparseTree(expr ast.Expr, app *application.Application) (unparseResult, error) {
+	switch e := expr.(type) {
+	case *ast.BinaryExpr:
+		left, err := unparseTree(e.X, app)
+		if err != nil {
+			return unparseResultEmpty, err
+		}
+
+		right, err := unparseTree(e.Y, app)
+		if err != nil {
+			return unparseResultEmpty, err
+		}
+
+		op := operation.Operation{}
+
+		if left.OperationID != 0 {
+			op.LeftOperationID = left.OperationID
+		} else {
+			op.Left = left.Value
+		}
+
+		if right.OperationID != 0 {
+			op.RightOperationID = right.OperationID
+		} else {
+			op.Right = right.Value
+		}
+
+		switch e.Op {
+		case token.ADD:
+			op.Op = operation.Addition
+		case token.SUB:
+			op.Op = operation.Subtraction
+		case token.MUL:
+			op.Op = operation.Multiply
+		case token.QUO:
+			op.Op = operation.Division
+		default:
+			return unparseResultEmpty, fmt.Errorf("unsupported operation: %s", e.Op)
+		}
+
+		opID, err := app.Database.Create(op)
+
+		if err != nil {
+			return unparseResultEmpty, err
+		}
+
+		return unparseResult{OperationID: opID}, nil
+	case *ast.BasicLit:
+		value, err := strconv.ParseFloat(e.Value, 64)
+		if err != nil {
+			return unparseResultEmpty, err
+		}
+
+		return unparseResult{Value: value}, nil
+	case *ast.ParenExpr:
+		return unparseTree(e.X, app)
+	default:
+		return unparseResultEmpty, fmt.Errorf("unsupported expression type: %T", e)
+	}
 }
